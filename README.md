4fa20bd294bbfcbc7048a0fc361fcc14b2cf75a3
abdelhadi-nofal <hadinofal1993@gmail.com>
2021-04-26 18:54:18 +0300

    second-push

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # City-Explorer
+
+Overview
+First BackEnd Project
+Lab-07
+
+***
+
+Getting Started
+Setting up repo
+Locations
+Weather
+Errors
+Architecture
+Node.js JavaScript
+
+***
+
+Feature 1: Locations
+Estimate of time needed to complete: 1 hour
+
+Start time: 1:20pm
+
+Finish time: 2:15pm
+
+Actual time needed to complete: 55 minutes
+
+Feature 2: Weather
+Estimate of time needed to complete: 1 hour
+
+Start time: 2:15pm
+
+Finish time: 3:00pm
+
+Actual time needed to complete: 45 minutes
+
+Feature 3: ERROR
+Estimate of time needed to complete: 30 minutes
+
+Start time: 3:00pm
+
+Finish time: 3:15pm
+
+Actual time needed to complete: 15 minutes
+
+25-04-2021 4:15pm
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 'use strict';
 
+// Aplication Setup
+
 const express = require('express');
 require('dotenv').config();
 
@@ -8,59 +10,169 @@ const cors = require('cors');
 
 server.use(cors());
 
+const superagent = require('superagent');
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
-server.get('/',(req,res) =>{
+// Routes Definition
+
+server.get('/',homeHandler);
+server.get('/location',locationHandler);
+server.get('/weather',weatherHandler);
+server.get('/parks',parksHandler);
+server.use(erorrHandler);
+
+// Functions
+
+function homeHandler(req,res){
   res.send('you are live');
-});
-
-
-
-
-
-server.get('/location',(req,res) =>{
-  let locationData = require('./data/location.json');
-  let locationRes = new Location(locationData);
-  res.send(locationRes);
-
-});
-
-
-
-
-function Location(locData){
-  this.search_query = locData[0].display_name.split(',')[0];
-  this.formatted_query = locData[0].display_name;
-  this.latitude = locData[0].lat;
-  this.longitude = locData[0].lon;
 }
+
+function locationHandler(req,res){
+  const cityName = req.query.city;
+  let key = process.env.GEOCODE_API_KEY ;
+  let URL = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${cityName}&format=json`;
+
+  superagent.get(URL)
+    .then(apiData =>{
+      const locationData  = new Location (cityName ,apiData.body);
+      res.send(locationData );
+
+    })
+    .catch((err) =>{
+      res.send(err);
+    });
+
+}
+
+function weatherHandler(req,res){
+  let city = req.query.city;
+  console.log(city);
+  let key = process.env.WEATHER_API_KEY;
+  let URL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${key}`;
+
+  superagent.get(URL)
+    .then(weatherData => {
+      let weatherArr = weatherData.body.data.map(val => new Weather(val));
+      res.send(weatherArr);
+
+    })
+
+    .catch((err) =>{
+      res.send(err);
+    });
+
+
+}
+
+function parksHandler(req,res){
+  let cityName = req.query.city;
+  console.log(cityName);
+  let key = process.env.PARKS_API_KEY;
+
+  let URL = `https://developer.nps.gov/api/v1/parks?q=${cityName}&api_key=${key}`;
+
+  superagent.get(URL)
+    .then(parksData => {
+      console.log(parksData);
+      let parksArr = parksData.body.data.map(val => new Parks(val));
+      res.send(parksArr);
+    })
+
+    .catch((err) =>{
+      res.send(err);
+    });
+}
+
+function erorrHandler(req,res){
+  res.status(500).send('Sorry, something went wrong');
+}
+
+// Constructor Functions for the data
+
+function Location(cityName,apiData){
+  this.search_query = cityName;
+  this.formatted_query = apiData[0].display_name;
+  this.latitude = apiData[0].lat;
+  this.longitude = apiData[0].lon;
+}
+
+function Weather(weatherData) {
+  this.forecast = weatherData.weather.description;
+  this.time = new Date(weatherData.valid_date).toString().slice(0, 15);
+
+
+}
+
+function Parks(parksData) {
+  this.name = parksData.name;
+  this.address = `"${parksData.addresses[0].line1}" "${parksData.addresses[0].city}" "${parksData.addresses[0].stateCode}" "${parksData.addresses[0].postalCode}"`;
+  // this.fee = parkData.entranceFees[0].cost;
+  this.fee = '0.00';
+  this.description = parksData.description;
+  this.url = parksData.url;
+}
+
+//////////////////////////////////////////////////////
+
 
 server.listen(PORT,()=>{
   console.log(`listening to ${PORT}`);
 });
 
 
-server.get('/weather',(req, res) =>{
-  const weatherData = require('./data/weather.json');
-  let weatherArr = [];
-  weatherData.data.forEach(item =>{
-    const weather = new Weather(item.weather.description, item.valid_date);
-    weatherArr.push(weather);
-  });
-  res.send(weatherArr);
-});
+
+//////////////////////////////////////////////////////////////////
 
 
 
-function Weather(forecast, time) {
-  this.forecast = forecast;
-  this.time = time;
-}
 
-server.get('*',(req,res) =>{
-  res.status(500).send('Sorry, something went wrong');
-});
+// server.get('/location',(req,res) =>{
+//   let cityName = req.query.city;
+//   console.log(cityName);
+//   let locationData = require('./data/location.json');
+//   let locationRes = new Location(locationData);
+//   res.send(locationRes);
+
+// });
+
+
+
+
+// function Location(locData){
+//   this.search_query = locData[0].display_name.split(',')[0];
+//   this.formatted_query = locData[0].display_name;
+//   this.latitude = locData[0].lat;
+//   this.longitude = locData[0].lon;
+// }
+
+
+
+
+
+// server.get('/weather',(req, res) =>{
+//   const weatherData = require('./data/weather.json');
+//   let weatherArr = [];
+//   weatherData.data.forEach(item =>{
+//     const weather = new Weather(item);
+//     weatherArr.push(weather);
+//   });
+//   res.send(weatherArr);
+// });
+
+
+// function Weather(weatherData) {
+//   this.forecast = weatherData.weather.description;
+//   this.time = weatherData.valid_date;
+// }
+
+// server.get('*',(req,res) =>{
+//   res.status(500).send('Sorry, something went wrong');
+// });
+
+
+
+
 
 
 
