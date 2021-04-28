60d855bc847ec2b6e59eee2139550c53f4e19e60
abdelhadi-nofal <hadinofal1993@gmail.com>
2021-04-28 21:21:37 +0300

    lab-8

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS locations;
 
 CREATE TABLE IF NOT EXISTS locations (
     search_query VARCHAR(255),
-    formatted_query VARCHAR(255)
-    -- latitude VARCHAR(255)
-    -- longitude VARCHAR(255)
+    formatted_query VARCHAR(255),
+    latitude NUMERIC(10, 7),
+    longitude NUMERIC(10, 7)
 );
--- server.js
+++ server.js
@@ -23,6 +23,7 @@ server.get('/',homeHandler);
 server.get('/location',locationHandler);
 server.get('/weather',weatherHandler);
 server.get('/parks',parksHandler);
+server.get('/movies',moviesHandler);
 server.use(erorrHandler);
 
 // Functions
@@ -31,20 +32,47 @@ function homeHandler(req,res){
   res.send('you are live');
 }
 
+// function locationHandler(req,res){
+//   const cityName = req.query.city;
+//   let key = process.env.GEOCODE_API_KEY ;
+//   let URL = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${cityName}&format=json`;
+
+//   superagent.get(URL)
+//     .then(apiData =>{
+//       const locationData  = new Location (cityName ,apiData.body);
+//       res.send(locationData );
+
+//     })
+//     .catch((err) =>{
+//       res.send(err);
+//     });
+// }
+
 function locationHandler(req,res){
   const cityName = req.query.city;
   let key = process.env.GEOCODE_API_KEY ;
   let URL = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${cityName}&format=json`;
+  // let SQL = `SELECT * FROM locations`;
 
   superagent.get(URL)
     .then(apiData =>{
+      // console.log(apiData);
       const locationData  = new Location (cityName ,apiData.body);
-      res.send(locationData );
+      let SQLINSERT = `INSERT INTO locations (search_query,formatted_query,latitude,longitude)
+       VALUES ($1,$2,$3,$4) RETURNING *;`;
+      let safeValues = [locationData.search_query, locationData.formatted_query, locationData.latitude, locationData.longitude];
+      client.query(SQLINSERT,safeValues)
+        .then(data =>{
+          res.send(data.rows[0]);
+        });
+
 
     })
     .catch((err) =>{
       res.send(err);
     });
+
+
 
 }
 
@@ -52,6 +80,7 @@ function weatherHandler(req,res){
   let city = req.query.search_query;
   let key = process.env.WEATHER_API_KEY;
   let URL = `https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${key}&days=8`;
+
 
   superagent.get(URL)
     .then(weatherData => {
@@ -85,6 +114,27 @@ function parksHandler(req,res){
     });
 }
 
+//http://localhost:4000/movies?api_key=4d1d4aa4d4b6a6c928e854be86e02ba9&query=seattle&language=de-DE&region=DE
+function moviesHandler(req,res){
+  let city = req.query.query;
+  let key = process.env.MOVIE_API_KEY;
+  let URL = `https://api.themoviedb.org/3/search/movie?api_key=${key}&query=${city}&language=de-DE&region=DE`;
+
+
+  superagent.get(URL)
+    .then(moviesData => {
+      let moviesArr = moviesData.body.results.map(val => new Movies(val));
+      res.send(moviesArr);
+
+    })
+
+    .catch((err) =>{
+      res.send(err);
+    });
+
+
+}
+
 function erorrHandler(req,res){
   res.status(500).send('Sorry, something went wrong');
 }
@@ -112,22 +162,29 @@ function Parks(parksData) {
   this.url = parksData.url;
 }
 
+function Movies(moviesData) {
+  this.title = moviesData.title;
+  this.overview = moviesData.overview ;
+  this.vote_average = moviesData.vote_average;
+  this.vote_count = moviesData.vote_count;
+  this.poster_path = moviesData.poster_path;
+  this.popularity = moviesData.popularity;
+  this.release_date = moviesData.release_date;
+}
+
 //////////////////////////////////////////////////////
 
 
 
 client.connect()
   .then(() => {
-    server.listen(PORT, () =>
-      console.log(`listening on ${PORT}`)
-    );
+    server.listen(PORT, () =>{
+      console.log(`listening on ${PORT}`);
+    });
 
   });
 
 // server.listen(PORT,()=>{
 //   console.log(`listening to ${PORT}`);
 // });
-
-
-
 
